8452e6df87bef39d51ad762dd6a1f2dbefe06f80
Lomia Wu <rwu30@myseneca.ca>
2023-01-19 22:21:57 -0500

    implement adding a task feature

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,15 @@
 const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.getElementById("new-task-form") as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>("#new-task-description");
+
+form?.addEventListener("submit", (e) => {
+   e.preventDefault();
+   const task = input?.value;
+   if (task) {
+      const li = document.createElement("li");
+      li.innerText = task;
+      list?.appendChild(li);
+   }
+});
+
+// Path: src\index.ts
